b2339c68635b210ff1f7088ef8506d7dfac54bfd
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-01-17 12:15:03 -0500

    create database connection

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,20 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+import type { DrizzleD1Database } from 'drizzle-orm/d1';
+import * as schema from '$lib/db/schema';
+
 declare global {
 	namespace App {
-        interface Platform {
-            env: Env
-            cf: CfProperties
-            ctx: ExecutionContext
-        }
-    }
+		interface Platform {
+			env: {
+				DB: D1Database;
+			};
+		}
+		interface Locals {
+			db: DrizzleD1Database<typeof schema>;
+		}
+	}
 }
 
 export {};
