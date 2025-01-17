53b3d2b6c7867e7ece82d991eef3b2747ca011f4
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-01-17 12:29:07 -0500

    initialize user table

--- src/lib/db/schema/users.ts
+++ src/lib/db/schema/users.ts
@@ -1,7 +1,11 @@
 import { relations, type InferSelectModel } from 'drizzle-orm';
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const users = sqliteTable('users', {});
+export const users = sqliteTable('users', {
+	id: int().primaryKey({ autoIncrement: true }).notNull(),
+	name: text().notNull(),
+	email: text().notNull()
+});
 
 export const usersRelations = relations(users, ({ one, many }) => ({}));
 
