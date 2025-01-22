3a48fdd34a298e857d01593ddb53b096bbdf869c
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-01-22 10:19:00 -0500

    remove styling that translates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,9 @@
-<div class="center-container translate-up">
-  <div class="Welcome to">    
-        <li>Medication</li>
-        <li>Reminders</li>
-        <li>Personalisation</li>
-        <li>Calendars</li>
-        <li>ChillPill</li>
-        <li aria-hidden="true">Medication</li>
-  </div>
+<div>
+	<div>
+		<li>Medication</li>
+		<li>Reminders</li>
+		<li>Personalisation</li>
+		<li>Calendars</li>
+		<li>ChillPill</li>
+	</div>
 </div>
-
-<style>
-  .center-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh; /* Full viewport height */
-  }
-  .translate-up {
-    transform:translateY(-50px)
-  }
-</style>
