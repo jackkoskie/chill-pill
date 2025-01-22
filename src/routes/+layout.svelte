255394eee537f24f03fbfcb5a50750e1fd46c3be
ric-e <136038531+ric-e@users.noreply.github.com>
2025-01-22 09:24:28 -0500

    does not work the buttons are stupid

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,46 +7,46 @@
 	<div class="">
 	  <a class="btn btn-ghost text-3xl">ChillPill</a>
 	</div>
-  
+
 	<!-- Dark and Light Theme Toggle -->
 	<label class="flex cursor-pointer gap-2 me-auto">
-	  <svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="20"
-		height="20"
-		viewBox="0 0 24 24"
-		fill="none"
-		stroke="currentColor"
-		stroke-width="2"
-		stroke-linecap="round"
-		stroke-linejoin="round">
-		<circle cx="12" cy="12" r="5" />
-		<path
-		  d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-	  </svg>
-	  <input type="checkbox" value="luxury" class="toggle theme-controller" />
-	  <svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="20"
-		height="20"
-		viewBox="0 0 24 24"
-		fill="none"
-		stroke="currentColor"
-		stroke-width="2"
-		stroke-linecap="round"
-		stroke-linejoin="round">
-		<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-	  </svg>
+		<svg
+		  xmlns="http://www.w3.org/2000/svg"
+		  width="20"
+		  height="20"
+		  viewBox="0 0 24 24"
+		  fill="none"
+		  stroke="currentColor"
+		  stroke-width="2"
+		  stroke-linecap="round"
+		  stroke-linejoin="round">
+		  <circle cx="12" cy="12" r="5" />
+		  <path
+			d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+		</svg>
+		<input type="checkbox" value="luxury" class="toggle theme-controller" />
+		<svg
+		  xmlns="http://www.w3.org/2000/svg"
+		  width="20"
+		  height="20"
+		  viewBox="0 0 24 24"
+		  fill="none"
+		  stroke="currentColor"
+		  stroke-width="2"
+		  stroke-linecap="round"
+		  stroke-linejoin="round">
+		  <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+		</svg>
 	</label>
   
 	<!-- Dropdown Menu -->
 	<div class="flex-none gap-2">
-	  <div class="flex-none">
+	  <div class="flex-none gap-2">
 		<ul class="menu menu-horizontal px-1">
 		  <li>
 			<details>
 			  <summary>Menu</summary>
-			  <ul class="bg-base-100 rounded-t-none p-2">
+			  <ul class="bg-base-100 rounded-3xl p-2">
 				<li><a>Account</a></li>
 				<li><a>Settings</a></li>
 				<li><a>Notifications</a></li>
@@ -57,7 +57,7 @@
 		</ul>
 	  </div>
 	  <div class="dropdown dropdown-end">
-		<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+		<div tabindex="0" role="button" class="btn-ghost btn btn-circle avatar">
 		  <div class="w-10 rounded-full">
 			<img
 			  
@@ -82,5 +82,9 @@
 		</ul>
 	  </div>
 	</div>
-  </div>
+</div>
+  <!-- Navbar ends here -->
+  
+  <!-- Main content -->
+
 {@render children()}
