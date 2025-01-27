1e603e462ba129f1b702a2b6044eb2710e4b3f54
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-01-27 11:16:35 -0500

    stylize login with google

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,7 +2,22 @@
 	import type { PageData } from './$types';
 
 	let { data }: { data: PageData } = $props();
+
+	import Icon from '@iconify/svelte';
 </script>
 
-<h1>Sign in</h1>
-<a href="/login/google">Sign in with Google</a>
+<section class="">
+	<div class="my-auto flex min-h-screen w-full flex-col items-center justify-center">
+		<div class="card w-96 bg-base-300 shadow-xl">
+			<div class="card-body">
+				<div class="card-actions justify-center">
+					<button class="btn btn-neutral"
+						><Icon icon="devicon:google" /> Continue with Google</button
+					>
+				</div>
+			</div>
+		</div>
+		<!-- <h1>Sign in</h1>
+		<a href="/login/google">Sign in with Google</a> -->
+	</div>
+</section>
