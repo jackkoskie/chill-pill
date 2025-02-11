05349800b55a6e65828820107718b6aba030e4ea
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-02-11 11:54:20 -0500

    init paraglide

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { i18n } from '$lib/i18n';
+
 	import { onMount } from 'svelte';
 	import Icon from '@iconify/svelte';
 	import '../app.css';
@@ -20,103 +23,103 @@
 	});
 </script>
 
-<div class="flex min-h-screen flex-col">
-	<div class="navbar sticky top-0 z-[9999] bg-base-300">
-		<!-- Top left corner Website Title -->
-		<a href="/" class="btn btn-ghost mx-2 gap-2 px-1 text-3xl"
-			><img src={logo} class="w-12" />Chill Pill</a
-		>
+<ParaglideJS {i18n}>
+	<div class="flex min-h-screen flex-col">
+		<div class="navbar sticky top-0 z-[9999] bg-base-300">
+			<!-- Top left corner Website Title -->
+			<a href="/" class="btn btn-ghost mx-2 gap-2 px-1 text-3xl"
+				><img src={logo} class="w-12" />Chill Pill</a
+			>
 
-		<!-- Dark and Light Theme Toggle -->
-		<label class="label-info me-auto flex cursor-pointer gap-2">
-			<Icon icon="flowbite:sun-outline" style="font-size: 30px" />
-			<input
-				type="checkbox"
-				value="luxury"
-				class="theme-controller toggle"
-				bind:checked={theme}
-				onclick={toggleTheme}
-			/>
-			<Icon icon="flowbite:moon-outline" style="font-size: 30px" />
-		</label>
+			<!-- Dark and Light Theme Toggle -->
+			<label class="label-info me-auto flex cursor-pointer gap-2">
+				<Icon icon="flowbite:sun-outline" style="font-size: 30px" />
+				<input
+					type="checkbox"
+					value="luxury"
+					class="theme-controller toggle"
+					bind:checked={theme}
+					onclick={toggleTheme}
+				/>
+				<Icon icon="flowbite:moon-outline" style="font-size: 30px" />
+			</label>
 
-		<!-- Dropdown Menu -->
-		{#if data.user}
-			<div class="flex-none gap-2">
+			<!-- Dropdown Menu -->
+			{#if data.user}
 				<div class="flex-none gap-2">
-					<ul class="menu menu-horizontal p-1">
-						<li><a>Notifications</a></li>
-						<li><a>Calendar</a></li>
-					</ul>
-				</div>
-				<div class="dropdown dropdown-end">
-					<div class="btn btn-ghost" tabindex="0" role="button">
-						<span>{data.user.name}</span>
+					<div class="flex-none gap-2">
+						<ul class="menu menu-horizontal p-1">
+							<li><a>Notifications</a></li>
+							<li><a>Calendar</a></li>
+						</ul>
 					</div>
-					<ul
-						tabindex="0"
-						class="bg-base-400 menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box p-2 shadow"
-					>
-						<li>
-							<a class="justify-between"> Profile </a>
-						</li>
-						<li><a>Settings</a></li>
-						<li><a href="/logout">Log Out</a></li>
-					</ul>
-				</div>
-			</div>
-		{:else}
-			
-			<div class="flex-none gap-2">
-				<div class="dropdown dropdown-bottom">
-					<div class="btn btn-ghost" tabindex="0" role="button">
-						<span>Language</span>
+					<div class="dropdown dropdown-end">
+						<div class="btn btn-ghost" tabindex="0" role="button">
+							<span>{data.user.name}</span>
+						</div>
+						<ul
+							tabindex="0"
+							class="bg-base-400 menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box p-2 shadow"
+						>
+							<li>
+								<a class="justify-between"> Profile </a>
+							</li>
+							<li><a>Settings</a></li>
+							<li><a href="/logout">Log Out</a></li>
+						</ul>
 					</div>
-					<ul
-						tabindex="0"
-						class ="bg-base-400 menu dropdown-content menu-sm z-[1] mt-3 w-32 rounded-box p-2 shadow"
-					>
-						<li>
-							<a href="#" class="justify-between"> English </a>
-						</li>
-						<li><a href="#">Français</a></li>
-						<li><a href="#">中文</a></li>
-						<li><a href="#">Español</a></li>
-					</ul>
 				</div>
-				<a href="/login" class="btn btn-ghost">Login</a>
-			</div>
-		{/if}
+			{:else}
+				<div class="flex-none gap-2">
+					<div class="dropdown dropdown-bottom">
+						<div class="btn btn-ghost" tabindex="0" role="button">
+							<span>Language</span>
+						</div>
+						<ul
+							tabindex="0"
+							class="bg-base-400 menu dropdown-content menu-sm z-[1] mt-3 w-32 rounded-box p-2 shadow"
+						>
+							<li>
+								<a href="#" class="justify-between"> English </a>
+							</li>
+							<li><a href="#">Français</a></li>
+							<li><a href="#">中文</a></li>
+							<li><a href="#">Español</a></li>
+						</ul>
+					</div>
+					<a href="/login" class="btn btn-ghost">Login</a>
+				</div>
+			{/if}
+		</div>
+
+		<main class="flex-1">
+			{@render children()}
+		</main>
+
+		<footer class="footer z-[9999] items-center bg-neutral p-4 text-neutral-content">
+			<aside class="grid-flow-col items-center">
+				<p>
+					ChillPill | Copyright © 2024 - {new Date().getFullYear()} | All right reserved | Powered by
+					Cloudfare | <a href="/about" class="hover:link">About Us</a>
+				</p>
+			</aside>
+			<nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+				<a class="text-xl" href="https://en.wikipedia.org/wiki/LeBron_James" target="_blank">
+					<Icon icon="ion:basketball-sharp" />
+				</a>
+				<a class="text-xl" href="https://x.com" target="_blank">
+					<Icon icon="prime:twitter" />
+				</a>
+				<a class="text-xl" href="https://youtube.com" target="_blank">
+					<Icon icon="mdi:youtube" />
+				</a>
+				<a class="text-xl" href="https://instagram.com" target="_blank">
+					<Icon icon="mdi:instagram" />
+				</a>
+				<a class="text-xl" href="https://github.com/jackkoskie/chill-pill" target="_blank">
+					<Icon icon="mdi:github" />
+				</a>
+			</nav>
+		</footer>
 	</div>
-
-	<main class="flex-1">
-		{@render children()}
-	</main>
-
-	<footer class="footer z-[9999] items-center bg-neutral p-4 text-neutral-content">
-		<aside class="grid-flow-col items-center">
-			<p>
-				ChillPill | Copyright © 2024 - {new Date().getFullYear()} | All right reserved | Powered by
-				Cloudfare | <a href="/about" class="hover:link">About Us</a>
-			</p>
-		</aside>
-		<nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-			<a class="text-xl" href="https://en.wikipedia.org/wiki/LeBron_James" target="_blank">
-				<Icon icon="ion:basketball-sharp" />
-			</a>
-			<a class="text-xl" href="https://x.com" target="_blank">
-				<Icon icon="prime:twitter" />
-			</a>
-			<a class="text-xl" href="https://youtube.com" target="_blank">
-				<Icon icon="mdi:youtube" />
-			</a>
-			<a class="text-xl" href="https://instagram.com" target="_blank">
-				<Icon icon="mdi:instagram" />
-			</a>
-			<a class="text-xl" href="https://github.com/jackkoskie/chill-pill" target="_blank">
-				<Icon icon="mdi:github" />
-			</a>
-
-		</nav>
-	</footer>
-</div>
+</ParaglideJS>
