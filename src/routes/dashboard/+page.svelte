c4f340f169b9638e94b81b9d0ab52268d9530238
ric-e <136038531+ric-e@users.noreply.github.com>
2025-03-03 12:55:13 -0500

    working date system - need to implement clock

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,15 +2,32 @@
 	import type { PageData } from './$types';
 	import * as m from '$lib/paraglide/messages.js';
 	import { FakePrimitiveParam } from 'drizzle-orm';
+	import { intToDay } from '$lib/utils/converter';
 	let { data }: { data: PageData } = $props();
-    let date = new Date();
+	let date = new Date();
+	const month = date.toLocaleString('default', { month: 'long' });
+	const weekday = date.toLocaleString('default', { weekday: 'long' });
+	const day = date.toLocaleString('defauly', { day: 'numeric' });
+	function ordinal(date) {
+		if (date > 20 || date < 10) {
+			switch (date % 10) {
+				case 1:
+					return 'st';
+				case 2:
+					return 'nd';
+				case 3:
+					return 'rd';
+			}
+		}
+		return 'th';
+	}
 </script>
 
 <div class="text-center text-5xl">
-	<h1>Hello, User</h1>
-    <p>
-        {date.getDate()} / {date.getDay()} / {date.getFullYear()} / {date.getMonth()}
+	<h1>Welcome Back, User!</h1>
 
-    </p>
-    
+	<p>
+		Today is {weekday}, {month}
+		{day}{ordinal(day)}
+	</p>
 </div>
