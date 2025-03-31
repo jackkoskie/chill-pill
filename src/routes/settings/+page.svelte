9589ad2867e7e1f74b1f55fd34cd3e7b337eeddb
ric-e <136038531+ric-e@users.noreply.github.com>
2025-03-31 13:50:12 -0400

    fixed layering for checkbox

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -190,7 +190,7 @@
 									><Icon icon="lucide:edit" /></label
 								>
 								<input type="checkbox" id={med.id.toString()} class="modal-toggle" />
-								<div class="modal modal-middle" role="dialog">
+								<div class="modal modal-middle z-[9999]" role="dialog">
 									<div class="modal-box">
 										<h3 class="text-lg font-bold">{m.edit_medication({ 'med.name': med.name })}</h3>
 										<div class="mt-1 flex flex-col gap-3">
@@ -373,7 +373,7 @@
 
 			<!-- Put this part before </body> tag -->
 			<input type="checkbox" id="add_modal" class="modal-toggle" />
-			<div class="modal modal-middle" role="dialog">
+			<div class="modal modal-middle z-[9999]" role="dialog">
 				<div class="modal-box">
 					<h3 class="text-lg font-bold">Add Medication</h3>
 					<div class="mt-1 flex flex-col gap-3">
