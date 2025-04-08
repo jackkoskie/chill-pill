a995290c7ba1eba53bdd3c8b8118330f618fd204
ric-e <136038531+ric-e@users.noreply.github.com>
2025-04-08 13:30:52 -0400

    warning labels and pancakes

---
+++ src/routes/history/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import * as m from '$lib/paraglide/messages';
+
+	let { data }: { data: PageData } = $props();
+
+	const getGreeting = (name: string) => {
+		const date = new Date();
+		const hours = date.getHours();
+		if (hours < 12) {
+			return m.greeting_morning({ name });
+		} else if (hours < 18) {
+			return m.greeting_afternoon({ name });
+		} else {
+			return m.greeting_evening({ name });
+		}
+	};
+</script>
+
+<div class="container mx-auto">
+	<h1 class="pt-6 text-3xl font-semibold">{getGreeting(data.user!.name.split(' ')[0])}</h1>
+	<p class="my-1 text-sm text-gray-500">Use this page to view your medication history.</p>
+	<div class="mt-0 divider"></div>
+</div>
+
