f342548c84c006bf856d6c043769385ce1edfad7
Dush04 <86071473+Dush04@users.noreply.github.com>
2025-05-23 17:19:08 -0400

    Adding writable type in imports

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { onMount } from 'svelte';
-	import { writable, derived } from 'svelte/store';
+	import { writable, derived, type Writable } from 'svelte/store';
 	import { intToDay, intToTime } from '$lib/utils/converter';
 	import * as m from '$lib/paraglide/messages';
 
